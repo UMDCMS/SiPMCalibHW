c04a1f00cebd4e58d48abcd25b808788d505c215
Yi-Mu Chen <enochnotsocool@gmail.com>
2023-07-06 17:14:41 +0200

    Finalizing all design and documentation

--- Panel/kikit_script.py
+++ Panel/kikit_script.py
@@ -1,5 +1,6 @@
 #!/bin/bash 
 
+# Panel generation
 common=""
 common="${common} --layout 'grid; rows: 2; cols: 2; space: 2mm'"
 common="${common} --tabs   'fixed; width: 3mm; vcount: 2'"
@@ -11,4 +12,20 @@ common="${common} --post 'millradius: 1mm'"
 eval kikit panelize "${common}" ../LEDPulser/LEDPulser.kicad_pcb                    panel_LEDPulser.kicad_pcb
 eval kikit panelize "${common}" ../RPiHat/RPiHatHVLVPower/RPiHatHVLVPower.kicad_pcb panel_HVLVPower.kicad_pcb
 eval kikit panelize "${common}" ../RPiHat/RPiHatSensor/RPiHatSensor.kicad_pcb       panel_Sensor.kicad_pcb
+
+# Generating the PCB manufacturing zip files
+for BOARD in LEDPulser HVLVPower Sensor; do 
+  # Generating the gerber files
+  for layer in F.Cu B.Cu F.Paste B.Paste F.Silkscreen B.Silkscreen F.Mask B.Mask Edge.Cuts  ; do
+    eval kicad-cli pcb export gerber --layers=$layer -o panel_${BOARD}_${layer}.gbr panel_${BOARD}.kicad_pcb
+  done
+
+  # Generating the drill files 
+  eval kicad-cli pcb export drill panel_${BOARD}.kicad_pcb
+
+  # Generating the zip files and cleaning up individual gerber files
+  rm -rf panel_${BOARD}.zip
+  zip panel_${BOARD}.zip panel_${BOARD}_*.gbr panel_${BOARD}.drl
+  rm panel_${BOARD}_*.gbr panel_${BOARD}.drl
+done 
 
